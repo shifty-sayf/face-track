f504776ee8639074923804d8713e617e4b81fe42
sayf-ismail <63899527+sayf-ismail@users.noreply.github.com>
2024-02-20 12:15:04 +0800

    got video working

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,50 @@
 import { useEffect } from 'react';
 import './App.css';
-import { Canvas, useFrame, useGraph } from '@react-three/fiber';
-import { Color, Euler, Matrix4, SkinnedMesh } from 'three'
-import { useGLTF } from '@react-three/drei';
-import { FilesetResolver, FaceLandmarker, FaceLandmarkerOptions, Category } from '@mediapipe/tasks-vision';
+import { FilesetResolver, FaceDetectorOptions, FaceDetector, Detection } from '@mediapipe/tasks-vision';
 
 let video: HTMLVideoElement;
-let faceLandmarker: FaceLandmarker;
+let faceDetector: FaceDetector;
+let children: any[] = [];
 let lastVideoTime = -1;
-let headMesh: SkinnedMesh;
-let rotation: Euler;
-let blendshapes: Category[] = [];
+const liveView = document.getElementById("liveView");
+video = document.getElementById("video") as HTMLVideoElement;
 
 function App() {
 
-  const handleOnChange = () => {
-
-  }
-
-  const options: FaceLandmarkerOptions = {
+  const options: FaceDetectorOptions = {
     baseOptions: {
-      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
       delegate: "GPU"
     },
-    numFaces: 1,
-    runningMode: "VIDEO",
-    outputFaceBlendshapes: true,
-    outputFacialTransformationMatrixes: true,
+    runningMode: "VIDEO"
   };
 
   const setup = async () => {
     const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
-    faceLandmarker = await FaceLandmarker.createFromOptions(vision, options);
+    faceDetector = await FaceDetector.createFromOptions(
+      vision,
+      options
+    )
 
-    video = document.getElementById("video") as HTMLVideoElement;
     navigator.mediaDevices.getUserMedia({
       video: { width: 1280, height: 720 },
       audio: false
     }).then((stream) => {
-      video.srcObject = stream
+      console.log(stream.getTracks()[0])
+      if (stream) video.srcObject = new MediaStream([stream.getTracks()[0]])
       video.addEventListener('loadeddata', predict);
+    }).catch((err) => {
+      console.error(err);
     })
   }
 
-  const predict = () => {
+  const predict = async () => {
     let nowInMs = Date.now();
     if (lastVideoTime !== video.currentTime) {
       lastVideoTime = video.currentTime;
-      const result = faceLandmarker.detectForVideo(video, nowInMs)
+      const detections = faceDetector.detectForVideo(video, nowInMs).detections;
 
-      if (result.facialTransformationMatrixes && result.facialTransformationMatrixes.length > 0 && result.faceBlendshapes[0].categories) {
-        blendshapes = result.faceBlendshapes[0].categories
-        const matrix = new Matrix4().fromArray(result.facialTransformationMatrixes![0].data)
-        rotation = new Euler().setFromRotationMatrix(matrix);
-      }
-
+      displayVideoDetections(detections);
     }
 
     requestAnimationFrame(predict)
@@ -66,57 +56,57 @@ function App() {
 
   return (
     <div className="App">
-      <input type="text" placeholder='Enter your RPM avatar URL' onChange={handleOnChange} />
-      <video autoPlay id='video' />
-      <Canvas style={{
-        backgroundColor: 'purple',
-        height: 400,
-      }}
-        camera={{
-          fov: 25
-        }}
-        shadows
-      >
-        <ambientLight intensity={0.5} />
-        <pointLight position={[1, 1, 1]} color={new Color(1, 1, 0)} intensity={0.5} />
-        <pointLight position={[-1, 0, 1]} color={new Color(1, 0, 0)} intensity={0.5} />
-        <Avatar />
-      </Canvas>
+      <div id='liveView' className='videoView'>
+        <video autoPlay playsInline id='video' />
+      </div>
     </div>
   );
 }
 
-function Avatar() {
-  const avatar = useGLTF('https://models.readyplayer.me/65d4019a39b888fbc8cb3866.glb?morphTargets=ARKit&textureAtlas=1024')
-  const { nodes } = useGraph(avatar.scene)
+function displayVideoDetections(detections: Detection[]) {
+  // Remove any highlighting from previous frame.
 
-  console.log(nodes);
+  if (liveView !== null) {
+    for (let child of children) {
+      liveView.removeChild(child);
+    }
+    children.splice(0);
+  }
 
-  useEffect(() => {
-    headMesh = nodes.Wolf3D_Avatar as SkinnedMesh
-  }, [nodes])
+  // Iterate through predictions and draw them to the live view
+  for (let detection of detections) {
+    const p = document.createElement("p");
 
-  useFrame((_, delta) => {
-    if (headMesh !== null) {
-      blendshapes.forEach((blendshape) => {
-        let index = headMesh.morphTargetDictionary![blendshape.categoryName]
-        if (index >= 0) {
-          headMesh.morphTargetInfluences![index] = blendshape.score;
-        }
-      })
+    const highlighter = document.createElement("div");
+    highlighter.setAttribute("class", "highlighter");
 
+    if (detection.boundingBox) {
+      highlighter.style.left = `${video.offsetWidth -
+        detection.boundingBox.width -
+        detection.boundingBox.originX}px`
+      highlighter.style.top = `${detection.boundingBox.originY}px`
+      highlighter.style.width = `${detection.boundingBox.width - 10}px`
+      highlighter.style.height = `${detection.boundingBox.height}px`
     }
 
-    if (blendshapes.length > 0) {
-
-      nodes.Head.rotation.set(rotation?.x, rotation.y, rotation.z);
-      nodes.Neck.rotation.set(rotation?.x / 5 + 0.3, rotation.y / 5, rotation.z / 5);
-      nodes.Spine2.rotation.set(rotation?.x / 10, rotation.y / 10, rotation.z / 10);
+    if (liveView !== null) {
+      liveView.appendChild(highlighter);
+      liveView.appendChild(p);
     }
-  })
 
-
-  return <primitive object={avatar.scene} position={[0, -1.75, 3]} />
+    // Store drawn objects in memory so they are queued to delete at next call
+    children.push(highlighter);
+    children.push(p);
+    for (let keypoint of detection.keypoints) {
+      const keypointEl = document.createElement("spam");
+      keypointEl.className = "key-point";
+      keypointEl.style.top = `${keypoint.y * video.offsetHeight - 3}px`;
+      keypointEl.style.left = `${video.offsetWidth - keypoint.x * video.offsetWidth - 3
+        }px`;
+      if (liveView !== null) liveView.appendChild(keypointEl);
+      children.push(keypointEl);
+    }
+  }
 }
 
 export default App;
